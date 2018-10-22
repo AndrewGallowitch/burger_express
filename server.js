9979eb55852a6d4c57390bd225c2f1ea454d69a6
Drew Ross <42723693+AndrewGallowitch@users.noreply.github.com>
2018-10-22 15:16:31 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -14,15 +14,21 @@ app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
 
-connection.connect(function(err) {
+
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -31,52 +37,52 @@ connection.connect(function(err) {
 });
 
 
-app.get("/", function(req, res) {
-    var burg = [];
-    var devburg = [];
+app.get("/", function (req, res) {
+  var burg = [];
+  var devburg = [];
 
-    connection.query("SELECT * FROM burgers", function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
-for (var i = 0; i < data.length; i++) {
-    if (data[i].devoured) {
-        devburg.push(data[i]);
-    } else {
-        burg.push(data[i]);
+  connection.query("SELECT * FROM burgers", function (err, data) {
+    if (err) {
+      return res.status(500).end();
     }
-};
-      res.render("index", { burgers: burg, devBurgers: devburg });
-    });
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].devoured) {
+        devburg.push(data[i]);
+      } else {
+        burg.push(data[i]);
+      }
+    };
+    res.render("index", { burgers: burg, devBurgers: devburg });
   });
+});
 
-  app.post("/", function(req, res) {
-      console.log(req.body.burger_name)
-    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", req.body.burger_name, function(err, result) {
-      if (err) {
-        return res.status(500).end();
-      }
-  
-      res.json({ id: result.insertId });
-      console.log({ id: result.insertId });
-    });
-  });
+app.post("/", function (req, res) {
+  console.log(req.body.burger_name)
+  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", req.body.burger_name, function (err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
 
-  app.put("/", function(req, res) {
-    connection.query("UPDATE burgers SET devoured = true WHERE id = ?", [req.body.id], function(err, result) {
-      if (err) {
-        // If an error occurred, send a generic server failure
-        return res.status(500).end();
-      }
-      else if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-    });
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
   });
-  
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+});
+
+app.put("/", function (req, res) {
+  connection.query("UPDATE burgers SET devoured = true WHERE id = ?", [req.body.id], function (err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
   });
+});
+
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
